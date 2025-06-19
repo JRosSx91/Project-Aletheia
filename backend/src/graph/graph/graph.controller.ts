e9d779794f7da437314241e437196da8f6c3bbfc
JRosSx91 <hazee.1491@gmail.com>
2025-06-19 23:54:49 +0200

    creating graph controller first endpoint

--- backend/src/graph/graph/graph.controller.ts
+++ backend/src/graph/graph/graph.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { GraphService } from './graph.service';
 
 @Controller('graph')
-export class GraphController {}
+export class GraphController {
+  constructor(private readonly graphService: GraphService) {}
+
+  @Get()
+  findFullGraph() {
+    console.log('GraphController: Request received in GET /graph');
+    return this.graphService.getFullGraph();
+  }
+}
