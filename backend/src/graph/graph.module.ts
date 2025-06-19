4a33d9634d0d42a805c07959560ebb4b6952ef25
JRosSx91 <hazee.1491@gmail.com>
2025-06-19 16:25:42 +0200

    creating graph folder with basic service & controller

--- backend/src/graph/graph.module.ts
+++ backend/src/graph/graph.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Node } from './entities/node.entity';
 import { Connection } from './entities/connection.entity';
 import { SeederService } from './seeder/seeder.service';
+import { GraphService } from './graph/graph.service';
+import { GraphController } from './graph/graph.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Node, Connection])],
-  providers: [SeederService],
-  controllers: [],
+  providers: [SeederService, GraphService],
+  controllers: [GraphController],
 })
 export class GraphModule {}
