f1cf935ff3f646a622e56485ab262530d1454d47
JRosSx91 <hazee.1491@gmail.com>
2025-06-19 15:47:53 +0200

    creating .pnpmfile globally to manage build scripts

---
+++ .pnpmfile.cjs
@@ -0,0 +1,12 @@
+function readPackage(pkg) {
+  if (pkg.name === "better-sqlite3") {
+    pkg.requiresBuild = true;
+  }
+  return pkg;
+}
+
+module.exports = {
+  hooks: {
+    readPackage,
+  },
+};
