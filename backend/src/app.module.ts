a24996b408287c430a67f12fe2c7ecf3aef7970b
JRosSx91 <hazee.1491@gmail.com>
2025-06-19 14:26:18 +0200

    creating graph module basic structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphModule } from './graph/graph.module';
-
+import { Connection } from './graph/entities/connection.entity';
+import { Node } from './graph/entities/node.entity';
 @Module({
-  imports: [GraphModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'aletheia.db',
+      entities: [Node, Connection],
+      synchronize: true,
+    }),
+    GraphModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/graph/graph.module.ts
+++ backend/src/graph/graph.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Node } from './entities/node.entity';
+import { Connection } from './entities/connection.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Node, Connection])],
+  providers: [],
+  controllers: [],
+})
 export class GraphModule {}
