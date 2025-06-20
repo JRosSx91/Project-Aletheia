50858b14a1e05fc5e733bab0af6f98053bc0e7af
JRosSx91 <hazee.1491@gmail.com>
2025-06-20 19:28:44 +0200

    installing axios, creating basic frontend structure & its first interfaces

---
+++ frontend/src/types/graph.types.ts
@@ -0,0 +1,17 @@
+export interface Node {
+	id: string;
+	title: string;
+	description?: string;
+}
+
+export interface Connection {
+	id: string;
+	type: string;
+	fromNode: Node;
+	toNode: Node;
+}
+
+export interface GraphData {
+	nodes: Node[];
+	connections: Connection[];
+}
